70ddadd722d0439ca15376e6c77a97e1cd4423c2
Loick Cherimont <loickcherimont@gmail.com>
2025-08-20 22:50:02 +0200

    feat: add TicketModal component for displaying ticket details
    - Introduce the `TicketModal` component to show detailed information about a selected ticket.
    - Implement a dialog interface with fields for ticket ID, title, description, status, and solution, all in a disabled state for viewing purposes.

---
+++ src/components/TicketModal.tsx
@@ -0,0 +1,62 @@
+// TicketModal.tsx
+import {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogDescription,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea"
+import type { Ticket } from "@/types/Ticket";
+
+interface TicketModalProps {
+    ticket: Ticket;
+}
+
+export function TicketModal({ ticket }: TicketModalProps) {
+    return (
+        <Dialog>
+            <DialogTrigger className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                Details
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-lg w-full">
+                <DialogHeader>
+                    <DialogTitle>Ticket Details</DialogTitle>
+                    <DialogDescription>
+                        Details of ticket ID #{ticket.id}
+                    </DialogDescription>
+                </DialogHeader>
+
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-1 gap-2">
+                        <Label htmlFor="ticketID">ID</Label>
+                        <Input value={ticket.id} disabled  id="ticketID"/>
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-2">
+                        <Label htmlFor="ticketTitle">Title</Label>
+                        <Input value={ticket.title} disabled id="ticketTitle"/>
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-2">
+                        <Label htmlFor="ticketDescription">Description</Label>
+                        <Textarea placeholder="Type your message here." value={ticket.description} disabled id="ticketDescription"/>
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-2">
+                        <Label htmlFor="ticketStatus">Status</Label>
+                        <Input value={ticket.status} disabled id="ticketStatus"/>
+                    </div>
+
+                    <div className="grid grid-cols-1 gap-2">
+                        <Label htmlFor="ticketSolution">Solution</Label>
+                        <Input value={ticket.solution || "—"} disabled id="ticketSolution"/>
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+}
